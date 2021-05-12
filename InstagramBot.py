b00d46818f9e29173f307cf344a77f0a8d695ebe
khalilmcfarlane <kmac36960@gmail.com>
2021-05-12 13:28:23 -0400

    Update InstagramBot.py

--- InstagramBot.py
+++ InstagramBot.py
@@ -11,10 +11,11 @@ session.set_quota_supervisor(enabled=True, peak_comments_daily=200, peak_comment
                                  peak_likes_hourly=20, sleep_after=['likes', 'follows'])
 session.set_relationship_bounds(enabled=True,
                                     delimit_by_numbers=True,
-                                    max_followers=20000,
-                                    min_followers=500,
+                                    max_followers=1000000,
+                                    min_followers=100,
                                     min_following=50)
 session.like_by_feed(amount=20, randomize=5)
-session.like_by_users(["donkurayami", "von.allensworth"], amount=5)
+session.like_by_users(["donkurayami"], amount=5)
+#session.follow_user_followers(["khalil.mcfarlane"], amount=5)
 
 session.end()
